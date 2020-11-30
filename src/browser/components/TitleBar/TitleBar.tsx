7b84baf1fe681168a1562a4a6b6d124d97ef8275
Josselin BUILS <josselin.buils@gmail.com>
2020-11-30 22:23:53 +0100

    Toggle maximize when double click on title bar

--- src/browser/components/TitleBar/TitleBar.tsx
+++ src/browser/components/TitleBar/TitleBar.tsx
@@ -1,11 +1,28 @@
 import cn from 'classnames';
 import React, { FC } from 'react';
 import styles from './TitleBar.module.scss';
+import { SharedProperties } from '../../../shared/SharedProperties';
 
 export const TitleBar: FC<Props> = ({ className }) => (
-  <header className={cn(styles.titleBar, className)} />
+  <header
+    className={cn(styles.titleBar, className)}
+    onDoubleClick={toggleMaximize}
+  />
 );
 
 interface Props {
   className: string;
+}
+
+function toggleMaximize() {
+  const electronWindow = (window as SharedProperties).getCurrentElectronWindow?.();
+
+  if (electronWindow === undefined) {
+    return;
+  }
+  if (electronWindow.isMaximized()) {
+    electronWindow.unmaximize();
+  } else {
+    electronWindow.maximize();
+  }
 }
